abe5017b7c14e4f7c6df51d30bd904a7245cec98
Paulo Vital <94381670+PauloVtl@users.noreply.github.com>
2022-01-22 21:48:54 -0300

    novos arquivos selecionados

---
+++ frontend/src/components/store/reducers/product.js
@@ -0,0 +1,137 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Iphone 13 Pro Azul 128 GB",
+      price: 9450,
+      image: "./images/produtos/iphone-13-pro-blue.jpg",
+      id_categorys: 2,
+      name_categorys: "Iphones",
+      selected: false
+    },
+    {
+        id_product: 2,
+        category: "Smartphones",
+        fk_idcategorys: 1,
+        name_product: "Iphone 13 Pro Silver 128 GB",
+        price: 9450,
+        image: "./images/produtos/iphone-13-pro-silver.jpg",
+        id_categorys: 2,
+        name_categorys: "Iphones",
+        selected: false
+    },
+    {
+      id_product: 3,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Iphone 13 Azul 128 GB",
+      price: 6669,
+      image: "./images/produtos/iphone-13-azul.jpg",
+      id_categorys: 2,
+      name_categorys: "Iphones",
+      selected: false
+    },
+    {
+      id_product:4,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product:  "Iphone 13 Rosa 128 GB",
+      price: 6669,
+      image: "./images/produtos/iphone-13-rosa.jpg",
+      id_categorys: 2,
+      name_categorys: "Iphones",
+      selected: false
+    },
+    
+    {
+      id_product: 5,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Samsung S21 Ultra Preto 256 GB",
+      price: 5399.10,
+      image: "./images/produtos/samsung-S21-Ultra-preto.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Samsung S21 Plus Roxo 256 GB",
+      price: 4649,
+      image: "./images/produtos/Samsung-S21-Plus-roxo.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Samsung S21 Plus Branco 256 GB",
+      price: 4649,
+      image: "./images/produtos/Samsung-S21-Plus-branco.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Samsung S21 Cinza 128 GB",
+      price: 3500,
+      image: "./images/produtos/Samsung-S21-cinza.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Motorola G100 Azul 256 GB",
+      price: 3059,
+      image: "./images/produtos/Motorola G100-256-Azul.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Motorola G100 Luminous Sky 256 GB",
+      price: 3059,
+      image: "./images/produtos/Motorola G100-256-Luminous.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category:"Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Motorola G9 Play  Azul 64 GB",
+      price: 1428,
+      image: "./images/produtos/Motorola-G9-Play-Azul.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Smartphones",
+      fk_idcategorys: 1,
+      name_product: "Motorola E7 Plus Bronze 64 GB",
+      price: 1092,
+      image: "./images/produtos/Motorola-E7-Plus-Bronze.jpg",
+      id_categorys: 1,
+      name_categorys: "Android",
+      selected: false
+    },
+];
+
+export default productsList;
